2b3b166314f326b6e628bfc1450c0f723ff1c094
arifovi22 <arifovi22@gmail.com>
2019-12-16 10:44:49 -0500

    validate the date of orderPage

--- src/test/java/mct/cucumber/steps/definations/Mct_order_steps.java
+++ src/test/java/mct/cucumber/steps/definations/Mct_order_steps.java
@@ -1,61 +1,84 @@
 package mct.cucumber.steps.definations;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.testng.Assert;
 
-import io.cucumber.datatable.DataTable;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import mct.cucumber.steps.supportcode.Mct_page_loader;
 
 public class Mct_order_steps {
 
-	Mct_page_loader page; 
-	
+	Mct_page_loader page;
+
 	public Mct_order_steps(Mct_page_loader page) {
 		this.page = page;
 	}
 
 	@When("I navigate to order page")
 	public void i_navigate_to_order_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new cucumber.api.PendingException();
-		
+		// Write code here that turns the phrase above into concrete actions
+		// throw new cucumber.api.PendingException();
+
 		page.getMyAccountPage().takeMetoMyOrderPage();
-		
+
 	}
 
-	@Then("I see a table with al lmy orderes")
+	// @Then("I see a table with al lmy orderes")
 	public void i_see_a_table_with_al_lmy_orderes() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new cucumber.api.PendingException();
-		
+		// Write code here that turns the phrase above into concrete actions
+		// throw new cucumber.api.PendingException();
+
 		boolean isActualOrderTableisExist = page.getMyOrdersPage().isOrderTableExist();
-		
-		Assert.assertEquals(isActualOrderTableisExist,true,"Failed To Find the Order History Table ");
-		
+
+		Assert.assertEquals(isActualOrderTableisExist, true, "Failed To Find the Order History Table ");
+
 	}
-	
-	
-	@Then("I exepct count {int} in my order table")
+
+	// @Then("I exepct count {int} in my order table")
 	public void i_exepct_count_in_my_order_table(int expCount) {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new cucumber.api.PendingException();
-		
-		// =>> Get the total number of rows from a web table 
-		int actualCount = page.getMyOrdersPage().getTotalTableRowsCount()-1;
-		
-		
-		
-		Assert.assertEquals(actualCount,expCount,"Failed To Find the Correct Number of!!!");
+		// Write code here that turns the phrase above into concrete actions
+		// throw new cucumber.api.PendingException();
 
+		// =>> Get the total number of rows from a web table
+		int actualCount = page.getMyOrdersPage().getTotalTableRowsCount() - 1;
+
+		Assert.assertEquals(actualCount, expCount, "Failed To Find the Correct Number of!!!");
+
+	}
+
+	@When("I search order by order date {string} and search")
+	public void i_search_order_by_order_date_and_and_search(String string) {
+		// page.getMyOrdersPage().searchOrder(string);
+		page.getMyOrdersPage().searchOrderbySelect(string);
+
+	}
+
+	@Then("I exepct to see the {string} date order result.")
+	public void i_exepct_to_see_the_date_order_result(String string) {
+		String actual = page.getMyOrdersPage().validateDate();
+		Assert.assertEquals(actual, string, "Not Found");
+	}
+
+	@When("I search order by date {string} and search")
+	public void i_search_order_by_date_and_search(String string) {
+		page.getMyOrdersPage().searchOrderbySelect(string);
+		
 		
 	}
 
+	@Then("I exepct to see {string}.")
+	public void i_exepct_to_see(String string) {
+		String actual = page.getMyOrdersPage().invalidateDate();
+		Assert.assertEquals(actual, string, "Not Found");
+		}
+
+	
+
+	@When("I search order by previous date {string} and search")
+	public void i_search_order_by_previous_date_and_search(String string) {
+		page.getMyOrdersPage().searchOrderbySelect(string);
+	}
 
 }
--- src/test/resources/AllTestFeatures/order.feature
+++ src/test/resources/AllTestFeatures/order.feature
@@ -1,15 +1,22 @@
 Feature: My Orders
   as a logged in user i  do all order related activities
 
-  Background: User on HOme Page
+  Background: User on Order Page
     Given User is on MCT home page
     When User complete login with id "user" password "pass"
     And I goto myaccount page
+    And I navigate to order page
+@test1
+  Scenario: user see correct ordere by valid date
+    When I search order by order date "12/27/2017" and search
+    Then I exepct to see the "12/27/2017" date order result.
 
-  #Scenario: user can see the order history
-    #And I navigate to order page
-    #Then I see a table with al lmy orderes
+@test2
+  Scenario: user will be found message by searching foroward date
+    When I search order by date "12/27/2019" and search
+    Then I exepct to see "No Results Found !!".
 
-  Scenario: user see correct count of place orderes
-	    And I navigate to order page
-			Then I exepct count 10 in my order table
+@test3
+  Scenario: user will be found message by searching previous date
+    When I search order by previous date "12/27/2016" and search
+    Then I exepct to see "No Results Found !!".
